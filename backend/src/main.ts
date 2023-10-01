7d23fd264bf67c062e7877e51455dc1f92f326f1
Eder Sampaio <edersampaio@outlook.com.br>
2023-10-01 10:05:28 -0300

    feat: install and implement uuid lib

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,19 @@
 import express from "express";
 import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
+const todos = [
+  {
+    id: uuidv4(),
+    text: "Study Clean Architecture",
+    isCompleted: false,
+  },
+];
 
 app.use(cors());
 
-app.get("/todos", async (req, res) => {
-  const todos = [
-    {
-      id: Date.now(),
-      text: "Study Clean Architecture",
-      isCompleted: false,
-    },
-  ];
-
+app.get("/todos", (req, res) => {
   res.json(todos);
 });
 
