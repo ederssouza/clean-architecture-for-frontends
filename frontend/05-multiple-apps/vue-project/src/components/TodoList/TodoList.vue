7e0b7ced5a0db2587a9a3442a1f26b3df9f37e2e
Eder Sampaio <edersampaio@outlook.com.br>
2023-10-01 17:06:04 -0300

    feat: implement request to API

--- frontend/05-multiple-apps/vue-project/src/components/TodoList/TodoList.vue
+++ frontend/05-multiple-apps/vue-project/src/components/TodoList/TodoList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
+import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { TodoInput } from "../TodoInput";
 import { TodoListItem } from "../TodoListItem";
@@ -29,6 +30,25 @@ function handleRemoveTask(taskId: string) {
 
   tasks.value = updatedTasks;
 }
+
+onMounted(() => {
+  async function fetchTodos() {
+    try {
+      const { data } = await axios.get("http://localhost:3000/todos");
+      tasks.value = [...tasks.value, ...data];
+
+      /**
+       * WARNING: catch was ignored in the test on purpose,
+       * don't do this in real applications
+       */
+      /* c8 ignore next 3 */
+    } catch (error) {
+      tasks.value = [];
+    }
+  }
+
+  fetchTodos();
+});
 </script>
 
 <template>
