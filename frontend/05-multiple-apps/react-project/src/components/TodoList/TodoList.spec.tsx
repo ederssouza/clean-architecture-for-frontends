37ba613e699caa0b273ec715b4351d1a8a30faad
Eder Sampaio <edersampaio@outlook.com.br>
2023-10-01 17:59:27 -0300

    feat: add 100% code coverage

--- frontend/05-multiple-apps/react-project/src/components/TodoList/TodoList.spec.tsx
+++ frontend/05-multiple-apps/react-project/src/components/TodoList/TodoList.spec.tsx
@@ -1,84 +1,67 @@
-import { describe, expect, it } from "vitest";
+import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import TodoList from "./TodoList";
 import { TodosGateway } from "../../gateways";
 
+window.alert = vi.fn();
+
 describe("<TodoList />", () => {
-  const todosGateway: TodosGateway = {
-    async getTodos() {
-      return [];
-    },
-  };
+  let todosGateway: TodosGateway;
 
-  describe("and the user adds an invalid task", () => {
-    it("should not create a new task", async () => {
-      await waitFor(() => render(<TodoList todosGateway={todosGateway} />));
-
-      const newTask = "     ";
-      const totalTasks = screen.getByTestId("total-tasks");
-      const input = screen.getByPlaceholderText(
-        "Type your task and press Enter"
-      );
-
-      fireEvent.change(input, { target: { value: newTask } });
-      fireEvent.submit(input);
-
-      expect(screen.queryByText(newTask)).not.toBeInTheDocument();
-      expect(totalTasks).toHaveTextContent("0");
-    });
+  afterEach(() => {
+    vi.clearAllMocks();
   });
 
-  describe("and the user adds a valid task", () => {
-    it("should create a new task", async () => {
+  describe("when API returns an error", () => {
+    beforeAll(() => {
+      todosGateway = {
+        async getTodos() {
+          throw new Error("An error has occurred");
+        },
+      };
+    });
+
+    it("should show an alert", async () => {
       await waitFor(() => render(<TodoList todosGateway={todosGateway} />));
 
-      const newTask = "Study JS";
       const totalTasks = screen.getByTestId("total-tasks");
-      const input = screen.getByPlaceholderText(
-        "Type your task and press Enter"
-      );
-
-      fireEvent.change(input, { target: { value: newTask } });
-      fireEvent.submit(input);
 
       await waitFor(() => {
-        expect(screen.getByText(newTask)).toBeInTheDocument();
-        expect(input).toHaveValue("");
-        expect(totalTasks).toHaveTextContent("1");
+        expect(totalTasks).toHaveTextContent("0");
+        expect(window.alert).toBeCalledTimes(1);
       });
     });
   });
 
-  describe("and the user switches task completion", () => {
-    it("should alternates task style", async () => {
-      await waitFor(() => render(<TodoList todosGateway={todosGateway} />));
-
-      const newTask = "Study JS";
-      const otherTask = "Study React";
-      const totalTasks = screen.getByTestId("total-tasks");
-      const input = screen.getByPlaceholderText(
-        "Type your task and press Enter"
-      );
-
-      fireEvent.change(input, { target: { value: newTask } });
-      fireEvent.submit(input);
-
-      fireEvent.change(input, { target: { value: otherTask } });
-      fireEvent.submit(input);
-
-      const firstTaskCheckbox = screen.getAllByRole("checkbox")[0];
-      const taskElement = screen.getByText(newTask);
-
-      expect(totalTasks).toHaveTextContent("2");
-      expect(taskElement).toHaveStyle("text-decoration: none");
-
-      fireEvent.click(firstTaskCheckbox);
-
-      expect(taskElement).toHaveStyle("text-decoration: line-through");
+  describe("when API returns a successful response", () => {
+    beforeAll(() => {
+      todosGateway = {
+        async getTodos() {
+          return [];
+        },
+      };
     });
 
-    describe("when the user clicks on delete task button", () => {
-      it("should remove task", async () => {
+    describe("and the user adds an invalid task", () => {
+      it("should not create a new task", async () => {
+        await waitFor(() => render(<TodoList todosGateway={todosGateway} />));
+
+        const newTask = "     ";
+        const totalTasks = screen.getByTestId("total-tasks");
+        const input = screen.getByPlaceholderText(
+          "Type your task and press Enter"
+        );
+
+        fireEvent.change(input, { target: { value: newTask } });
+        fireEvent.submit(input);
+
+        expect(screen.queryByText(newTask)).not.toBeInTheDocument();
+        expect(totalTasks).toHaveTextContent("0");
+      });
+    });
+
+    describe("and the user adds a valid task", () => {
+      it("should create a new task", async () => {
         await waitFor(() => render(<TodoList todosGateway={todosGateway} />));
 
         const newTask = "Study JS";
@@ -90,16 +73,66 @@ describe("<TodoList />", () => {
         fireEvent.change(input, { target: { value: newTask } });
         fireEvent.submit(input);
 
-        const taskElement = screen.queryByText(newTask);
-        const deleteButton = screen.getByRole("button", { name: "Remove" });
+        await waitFor(() => {
+          expect(screen.getByText(newTask)).toBeInTheDocument();
+          expect(input).toHaveValue("");
+          expect(totalTasks).toHaveTextContent("1");
+        });
+      });
+    });
 
-        expect(taskElement).toBeInTheDocument();
-        expect(totalTasks).toHaveTextContent("1");
+    describe("and the user switches task completion", () => {
+      it("should alternates task style", async () => {
+        await waitFor(() => render(<TodoList todosGateway={todosGateway} />));
 
-        fireEvent.click(deleteButton);
+        const newTask = "Study JS";
+        const otherTask = "Study React";
+        const totalTasks = screen.getByTestId("total-tasks");
+        const input = screen.getByPlaceholderText(
+          "Type your task and press Enter"
+        );
 
-        expect(taskElement).not.toBeInTheDocument();
-        expect(totalTasks).toHaveTextContent("0");
+        fireEvent.change(input, { target: { value: newTask } });
+        fireEvent.submit(input);
+
+        fireEvent.change(input, { target: { value: otherTask } });
+        fireEvent.submit(input);
+
+        const firstTaskCheckbox = screen.getAllByRole("checkbox")[0];
+        const taskElement = screen.getByText(newTask);
+
+        expect(totalTasks).toHaveTextContent("2");
+        expect(taskElement).toHaveStyle("text-decoration: none");
+
+        fireEvent.click(firstTaskCheckbox);
+
+        expect(taskElement).toHaveStyle("text-decoration: line-through");
+      });
+
+      describe("when the user clicks on delete task button", () => {
+        it("should remove task", async () => {
+          await waitFor(() => render(<TodoList todosGateway={todosGateway} />));
+
+          const newTask = "Study JS";
+          const totalTasks = screen.getByTestId("total-tasks");
+          const input = screen.getByPlaceholderText(
+            "Type your task and press Enter"
+          );
+
+          fireEvent.change(input, { target: { value: newTask } });
+          fireEvent.submit(input);
+
+          const taskElement = screen.queryByText(newTask);
+          const deleteButton = screen.getByRole("button", { name: "Remove" });
+
+          expect(taskElement).toBeInTheDocument();
+          expect(totalTasks).toHaveTextContent("1");
+
+          fireEvent.click(deleteButton);
+
+          expect(taskElement).not.toBeInTheDocument();
+          expect(totalTasks).toHaveTextContent("0");
+        });
       });
     });
   });
--- frontend/05-multiple-apps/vue-project/src/components/TodoList/TodoList.spec.ts
+++ frontend/05-multiple-apps/vue-project/src/components/TodoList/TodoList.spec.ts
@@ -1,42 +1,27 @@
-import { describe, expect, it } from "vitest";
+import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 import { fireEvent, render, screen, waitFor } from "@testing-library/vue";
 import TodoList from "./TodoList.vue";
-import { Task } from "../../entities";
 import { TodosGateway } from "../../gateways";
 
+window.alert = vi.fn();
+
 describe("<TodoList />", () => {
-  const todosGateway: TodosGateway = {
-    async getTodos(): Promise<Task[]> {
-      return [];
-    },
-  };
+  let todosGateway: TodosGateway;
 
-  describe("and the user adds an invalid task", () => {
-    it("should not create a new task", () => {
-      render(TodoList, {
-        global: {
-          provide: {
-            todosGateway,
-          },
-        },
-      });
-
-      const newTask = "     ";
-      const totalTasks = screen.getByTestId("total-tasks");
-      const input = screen.getByPlaceholderText(
-        "Type your task and press Enter"
-      );
-
-      fireEvent.update(input, newTask);
-      fireEvent.submit(input);
-
-      expect(screen.queryByText(newTask)).not.toBeInTheDocument();
-      expect(totalTasks).toHaveTextContent("0");
-    });
+  afterEach(() => {
+    vi.clearAllMocks();
   });
 
-  describe("and the user adds a valid task", () => {
-    it("should create a new task", async () => {
+  describe("when API returns an error", () => {
+    beforeAll(() => {
+      todosGateway = {
+        async getTodos() {
+          throw new Error("An error has occurred");
+        },
+      };
+    });
+
+    it("should show an alert", async () => {
       render(TodoList, {
         global: {
           provide: {
@@ -45,66 +30,50 @@ describe("<TodoList />", () => {
         },
       });
 
-      const newTask = "Study JS";
       const totalTasks = screen.getByTestId("total-tasks");
-      const input = screen.getByPlaceholderText(
-        "Type your task and press Enter"
-      );
-
-      fireEvent.update(input, newTask);
-      fireEvent.submit(input);
 
       await waitFor(() => {
-        expect(screen.getByText(newTask)).toBeInTheDocument();
-        expect(input).toHaveValue("");
-        expect(totalTasks).toHaveTextContent("1");
+        expect(totalTasks).toHaveTextContent("0");
+        expect(window.alert).toBeCalledTimes(1);
       });
     });
   });
 
-  describe("and the user switches task completion", () => {
-    it("should alternates task style", async () => {
-      render(TodoList, {
-        global: {
-          provide: {
-            todosGateway,
-          },
+  describe("when API returns a successful response", () => {
+    beforeAll(() => {
+      todosGateway = {
+        async getTodos() {
+          return [];
         },
-      });
+      };
+    });
 
-      const newTask = "Study JS";
-      const otherTask = "Study React";
-      const totalTasks = screen.getByTestId("total-tasks");
-      const input = screen.getByPlaceholderText(
-        "Type your task and press Enter"
-      );
+    describe("and the user adds an invalid task", () => {
+      it("should not create a new task", () => {
+        render(TodoList, {
+          global: {
+            provide: {
+              todosGateway,
+            },
+          },
+        });
 
-      fireEvent.update(input, newTask);
-      fireEvent.submit(input);
+        const newTask = "     ";
+        const totalTasks = screen.getByTestId("total-tasks");
+        const input = screen.getByPlaceholderText(
+          "Type your task and press Enter"
+        );
 
-      fireEvent.update(input, otherTask);
-      fireEvent.submit(input);
+        fireEvent.update(input, newTask);
+        fireEvent.submit(input);
 
-      await waitFor(() => {
-        expect(screen.getByText(newTask)).toBeInTheDocument();
-        expect(screen.getByText(otherTask)).toBeInTheDocument();
-      });
-
-      const firstTaskCheckbox = screen.getAllByRole("checkbox")[0];
-      const taskElement = screen.getByText(newTask);
-
-      expect(totalTasks).toHaveTextContent("2");
-      expect(taskElement).toHaveAttribute("class", "");
-
-      fireEvent.click(firstTaskCheckbox);
-
-      await waitFor(() => {
-        expect(taskElement).toHaveAttribute("class", "task-completed");
+        expect(screen.queryByText(newTask)).not.toBeInTheDocument();
+        expect(totalTasks).toHaveTextContent("0");
       });
     });
 
-    describe("when the user clicks on delete task button", () => {
-      it("should remove task", async () => {
+    describe("and the user adds a valid task", () => {
+      it("should create a new task", async () => {
         render(TodoList, {
           global: {
             provide: {
@@ -124,19 +93,88 @@ describe("<TodoList />", () => {
 
         await waitFor(() => {
           expect(screen.getByText(newTask)).toBeInTheDocument();
+          expect(input).toHaveValue("");
+          expect(totalTasks).toHaveTextContent("1");
+        });
+      });
+    });
+
+    describe("and the user switches task completion", () => {
+      it("should alternates task style", async () => {
+        render(TodoList, {
+          global: {
+            provide: {
+              todosGateway,
+            },
+          },
         });
 
-        const taskElement = screen.queryByText(newTask);
-        const deleteButton = screen.getByRole("button", { name: "Remove" });
+        const newTask = "Study JS";
+        const otherTask = "Study React";
+        const totalTasks = screen.getByTestId("total-tasks");
+        const input = screen.getByPlaceholderText(
+          "Type your task and press Enter"
+        );
 
-        expect(taskElement).toBeInTheDocument();
-        expect(totalTasks).toHaveTextContent("1");
+        fireEvent.update(input, newTask);
+        fireEvent.submit(input);
 
-        fireEvent.click(deleteButton);
+        fireEvent.update(input, otherTask);
+        fireEvent.submit(input);
 
         await waitFor(() => {
-          expect(taskElement).not.toBeInTheDocument();
-          expect(totalTasks).toHaveTextContent("0");
+          expect(screen.getByText(newTask)).toBeInTheDocument();
+          expect(screen.getByText(otherTask)).toBeInTheDocument();
+        });
+
+        const firstTaskCheckbox = screen.getAllByRole("checkbox")[0];
+        const taskElement = screen.getByText(newTask);
+
+        expect(totalTasks).toHaveTextContent("2");
+        expect(taskElement).toHaveAttribute("class", "");
+
+        fireEvent.click(firstTaskCheckbox);
+
+        await waitFor(() => {
+          expect(taskElement).toHaveAttribute("class", "task-completed");
+        });
+      });
+
+      describe("when the user clicks on delete task button", () => {
+        it("should remove task", async () => {
+          render(TodoList, {
+            global: {
+              provide: {
+                todosGateway,
+              },
+            },
+          });
+
+          const newTask = "Study JS";
+          const totalTasks = screen.getByTestId("total-tasks");
+          const input = screen.getByPlaceholderText(
+            "Type your task and press Enter"
+          );
+
+          fireEvent.update(input, newTask);
+          fireEvent.submit(input);
+
+          await waitFor(() => {
+            expect(screen.getByText(newTask)).toBeInTheDocument();
+          });
+
+          const taskElement = screen.queryByText(newTask);
+          const deleteButton = screen.getByRole("button", { name: "Remove" });
+
+          expect(taskElement).toBeInTheDocument();
+          expect(totalTasks).toHaveTextContent("1");
+
+          fireEvent.click(deleteButton);
+
+          await waitFor(() => {
+            expect(taskElement).not.toBeInTheDocument();
+            expect(totalTasks).toHaveTextContent("0");
+          });
         });
       });
     });
